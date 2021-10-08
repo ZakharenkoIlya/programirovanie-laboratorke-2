29ac6118bb020a05324febc296df4d53964157b8
Ilya <89971116+ZakharenkoIlya@users.noreply.github.com>
2021-10-08 14:53:56 +0300

    Add files via upload

---
+++ cвап2.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c,l;
+	cin >> a >> b >> c;
+	cout << "a=" << a << " b=" << b << " c=" << c << endl;
+	l = a;
+	a = b;
+	b = c;
+	c = l;
+	cout << "a=" << a << " b=" <<b << " c=" << c << endl;
+	system("pause");
+}
+
---
+++ сваап3.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c, d,l;
+    cin >> a >> b >> c >> d;
+    cout << "a=" << a << " b=" << b << " c=" << c << " d=" << d << endl;
+    l = a;
+    a = b;
+    b = c;
+    c = d;
+    d = l;
+    cout << "a=" << a << " b=" << b << " c=" << c << " d=" << d << endl;
+    system("pause");
+}
